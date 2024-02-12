f11b883d347166de075200d8772f8114ea04ad34
Amshu Belbase <amsubelbs@gmail.com>
2024-02-13 01:28:01 +0530

    Cleared Unwanted

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="homewrap">
+      <div className="leftnav">
+        <div className="projectDetails">
+          <img src="" alt="" />
+          <h1>Scoramp</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Home;
