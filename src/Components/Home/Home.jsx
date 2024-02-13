9617558c35d0c5bf5111a6fae50bf4bd43dd236d
Amshu Belbase <amsubelbs@gmail.com>
2024-02-14 00:55:27 +0530

    Responsive Till Table

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,9 +3,9 @@ import picture from "../Assets/1.png";
 import avatar from "../Assets/icon.png";
 import { BiSolidDashboard } from "react-icons/bi";
 import { AiOutlineFundProjectionScreen } from "react-icons/ai";
-import { BsListTask } from "react-icons/bs";
 import { TbProgress } from "react-icons/tb";
 import { IoCheckmarkDoneCircle } from "react-icons/io5";
+import { IoIosArrowDown } from "react-icons/io";
 import {
   IoMdAddCircle,
   IoIosNotifications,
@@ -27,7 +27,7 @@ const Home = () => {
         </div>
         <div className="menu">
           <AiOutlineFundProjectionScreen className="icon" />
-          <h2>Project Tracking</h2>
+          <h2>Tracking</h2>
         </div>
         <div className="menu">
           <RiTeamFill className="icon" />
@@ -46,7 +46,7 @@ const Home = () => {
         <div className="top-nav">
           <div className="menu menu1">
             <AiOutlineFundProjectionScreen className="icon" />
-            <h2>Project Tracking</h2>
+            <h2>Tracking</h2>
           </div>
           <div className="menu">
             <IoMdAddCircle className="icon" />
@@ -59,7 +59,7 @@ const Home = () => {
             <IoIosNotifications className="icon" />
           </div>
           <div className="menu-icon">
-            <img src={avatar} className="avatar" />
+            <img src={avatar} alt="avatar" className="avatar" />
           </div>
         </div>
         <div className="container">
@@ -88,6 +88,60 @@ const Home = () => {
                 </div>
                 <div className="second">5</div>
                 <div className="third">+5 from yesterday</div>
+              </div>
+            </div>
+            <div className="table-top">
+              <div className="head">Task Tracking</div>
+              <div className="taskstat-container">
+                <div className="task-stat">New</div>
+                <div className="task-stat">In progress</div>
+                <div className="task-stat">Completed</div>
+              </div>
+              <div class="dropdown">
+                <button class="dropbtn">
+                  Sort By <IoIosArrowDown className="icon" />
+                </button>
+                <div class="dropdown-content">
+                  <label>Date</label>
+                  <label>Alphabet</label>
+                </div>
+              </div>
+            </div>
+            <div className="task-table">
+              <div className="task-table-list">
+                <div className="title">Title</div>
+                <div className="given-to">Given To</div>
+                <div className="given-date">Given At</div>
+                <div className="deadline">Deadline</div>
+                <div className="complete-percent">Finish</div>
+              </div>
+              <div className="task-table-list">
+                <div className="title">This is a Title</div>
+                <div className="given-to">This is a Given To</div>
+                <div className="given-date">13/02/2023</div>
+                <div className="deadline">13/02/2023</div>
+                <div className="complete-percent">75%</div>
+              </div>
+              <div className="task-table-list">
+                <div className="title">This is a Title</div>
+                <div className="given-to">This is a Given To</div>
+                <div className="given-date">13/02/2023</div>
+                <div className="deadline">13/02/2023</div>
+                <div className="complete-percent">75%</div>
+              </div>
+              <div className="task-table-list">
+                <div className="title">This is a Title</div>
+                <div className="given-to">This is a Given To</div>
+                <div className="given-date">13/02/2023</div>
+                <div className="deadline">13/02/2023</div>
+                <div className="complete-percent">75%</div>
+              </div>
+              <div className="task-table-list">
+                <div className="title">This is a Title</div>
+                <div className="given-to">This is a Given To</div>
+                <div className="given-date">13/02/2023</div>
+                <div className="deadline">13/02/2023</div>
+                <div className="complete-percent">75%</div>
               </div>
             </div>
           </div>
