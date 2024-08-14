d8c945ab9cd28b9ed188c0657f3410d9393c7888
Amshu Belbase <amsubelbs@gmail.com>
2024-08-15 01:05:11 +0530

    remove slash

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["https://scoramp.vercel.app/"],
+    origin: ["https://scoramp.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
