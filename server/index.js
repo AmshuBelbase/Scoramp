f8941ab924aad935ada7bbbdefabe85e6e2bf823
Amshu Belbase <amsubelbs@gmail.com>
2024-08-15 12:01:12 +0530

    * origin

--- server/index.js
+++ server/index.js
@@ -5,11 +5,17 @@ import mongoose from "mongoose";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// app.use(
+//   cors({
+//     origin: ["https://scoramp.vercel.app"],
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     credentials: true,
+//   })
+// );
+
 app.use(
   cors({
-    origin: ["https://scoramp.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    credentials: true,
+    origin: "*",
   })
 );
 
